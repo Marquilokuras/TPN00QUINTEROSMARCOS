da2ec6fcecd873f04aefa3a6a77fd8d63b28f7c4
Marcos Quinteros <92806059+Marquilokuras@users.noreply.github.com>
2022-04-08 11:22:53 -0300

    TRABAJO PRÁCTICO

---
+++ src/main/java/ar/edu/unju/edu/Application/Tpn00QuinterosmarcosApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edu.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tpn00QuinterosmarcosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tpn00QuinterosmarcosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unju/edu/Application/Tpn00QuinterosmarcosApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edu.Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tpn00QuinterosmarcosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
