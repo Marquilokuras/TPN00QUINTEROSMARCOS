048e6f5403c29c2ce43beee8f4d381fe39c705b8
Marcos Quinteros <92806059+Marquilokuras@users.noreply.github.com>
2022-04-10 13:41:33 -0300

    Resulto patron de diseño en MVC

--- src/main/java/ar/edu/unju/edm/controller/CalculadoraController.java
+++ src/main/java/ar/edu/unju/edm/controller/CalculadoraController.java
@@ -28,5 +28,65 @@ public class CalculadoraController {
 		return "resultadoS";
 	}
 	
+	@GetMapping("/calculoResta")
+	public String getRestaPage(@RequestParam (name = "num1") int num1 , @RequestParam (name = "num2") int num2, Model model) {
+		int resultadoR = 0;
+		Calculadora nuevaCalculadora = new Calculadora();
+		nuevaCalculadora.setNumero1(num1);
+		nuevaCalculadora.setNumero2(num2);
+		nuevaCalculadora.ResolverResta();
+		resultadoR = nuevaCalculadora.ResolverResta();
+		model.addAttribute("resultadoR", resultadoR);
+		return "resultadoR";
+	}
+
+	@GetMapping("/calculoMulti")
+	public String getMultiPage(@RequestParam (name = "num1") int num1 , @RequestParam (name = "num2") int num2, Model model) {
+		int resultadoM = 0;
+		Calculadora nuevaCalculadora = new Calculadora();
+		nuevaCalculadora.setNumero1(num1);
+		nuevaCalculadora.setNumero2(num2);
+		nuevaCalculadora.ResolverMulti();
+		resultadoM = nuevaCalculadora.ResolverMulti();
+		model.addAttribute("resultadoM", resultadoM);
+		return "resultadoM";
+	}
+	
+	@GetMapping("/calculoDivision")
+	public String getDivisionPage(@RequestParam (name = "num1") int num1 , @RequestParam (name = "num2") int num2, Model model) {
+		int resultadoD = 0;
+		Calculadora nuevaCalculadora = new Calculadora();
+		nuevaCalculadora.setNumero1(num1);
+		nuevaCalculadora.setNumero2(num2);
+		nuevaCalculadora.ResolverDivision();
+		resultadoD = nuevaCalculadora.ResolverDivision();
+		model.addAttribute("resultadoD", resultadoD);
+		return "resultadoD";
+	}
+	
+	@GetMapping("/calculoSPotencia")
+	public String getPotenciaPage(@RequestParam (name = "num1") int num1 , @RequestParam (name = "num2") int num2, Model model) {
+		int resultadoP = 0;
+		Calculadora nuevaCalculadora = new Calculadora();
+		nuevaCalculadora.setNumero1(num1);
+		nuevaCalculadora.setNumero2(num2);
+		nuevaCalculadora.ResolverPotencia();
+		resultadoP = nuevaCalculadora.ResolverPotencia();
+		model.addAttribute("resultadoP", resultadoP);
+		return "resultadoP";
+	}
+	
+	@GetMapping("/calculoRaiz")
+	public String getRaizPage(@RequestParam (name = "num1") int num1 , @RequestParam (name = "num2") int num2, Model model) {
+		float resultadoRA = 0;
+		Calculadora nuevaCalculadora = new Calculadora();
+		nuevaCalculadora.setNumero1(num1);
+		nuevaCalculadora.setNumero2(num2);
+		nuevaCalculadora.ResolverRaiz();
+		resultadoRA = nuevaCalculadora.ResolverRaiz();
+		model.addAttribute("resultadoRA", resultadoRA);
+		return "resultadoRA";
+	}
+	
 
 }
